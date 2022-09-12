79785ce65f2a793feeea61c31c81efca109249c3
Chowdhury Tanvir Ahmed <CAhmed@spartaglobal.com>
2022-09-12 01:58:45 +0100

    Session 2

--- CSharpCorePractice/Program.cs
+++ CSharpCorePractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices;
 
 namespace CSharpCorePractice
 {
@@ -27,12 +28,90 @@ namespace CSharpCorePractice
             {
 
                    arrayofMultipleValues[i] = givenNumber * (i+1);
-                Console.Write(arrayofMultipleValues[i] + " ");
+                Console.WriteLine(arrayofMultipleValues[i] + " ");
             }
             
                 return arrayofMultipleValues;
 
         }
+        //reversing the case
+        public static string ReverseTheCase(string input)
+        {
+            string output = string.Empty;
+            char[] arr = input.ToCharArray();
+            for(int i = 0; i<input.Length; i++)
+            {
+                if (char.ToLower(input[i]) == input[i])
+                {
+                    output += char.ToUpper(input[i]);
+                }
+                else if (char.ToUpper(input[i]) == input[i])
+                {
+                    output+=char.ToLower(input[i]);
+                }
+            }
+            return output;
+        }
+        //checking the equality of value and type
+        public static bool CheckEquality(object a, object b)
+        {
+            if (a.Equals(b))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //return the index of all capital letters
+        public static List<int> IndexofAllCapitalLetters(string input)
+        {
+            List<int> indexOfCapitalLettters = new List<int>();
+         
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.ToUpper(input[i]) == input[i])
+                {
+                    
+                  indexOfCapitalLettters.Add(i);
+                  
+                   
+                }
+            }
+            for (int i = 0; i < indexOfCapitalLettters.Count; i++)
+            {
+                Console.WriteLine(indexOfCapitalLettters[i]);
+            }
+            return indexOfCapitalLettters;
+        }
+        //finding a spcific word
+        public static string findTheBomb(string bomb)
+        {
+            string modifiedToLower = bomb.ToLower();
+            if (modifiedToLower.Contains("bomb"))
+            {
+                return "Duck!!!";
+            }
+            else
+            {
+                return "There is no bomb, relax.";
+            }
+        }
+        //Convert All Array Items to String
+        public static string[] ConvertAllArrayItemstoString(object[]arr)
+        {
+            string[] integerToString = new string[arr.Length];
+            
+            for(int i = 0; i<arr.Length;i++)
+            {
+                integerToString[i] = arr[i].ToString();
+                Console.WriteLine(integerToString[i]);
+            }
+            return integerToString;
+
+        }
+
         static void Main(string[] args)
         {
             //finding number of true values in an array
@@ -45,7 +124,33 @@ namespace CSharpCorePractice
             int givenNumber = 17;
             int arrLength = 6;
             multiplesOfAGivenNumber(givenNumber, arrLength);
-            
+
+            //reversing the case
+            Console.WriteLine("reversing the case");
+            string input = "MANY THANKS";
+            Console.WriteLine(ReverseTheCase(input));
+
+            //checking the equality of value and type;
+            Console.WriteLine("checking the equality of value and type");
+            Console.WriteLine(CheckEquality(1, true));
+            Console.WriteLine(CheckEquality(0, "0"));
+            Console.WriteLine(CheckEquality(1, 1));
+            Console.WriteLine(CheckEquality("1", "a"));
+
+            //return the index of all capital letters
+            Console.WriteLine("return the index of all capital letters");
+            IndexofAllCapitalLetters("eDaBiT");
+
+            //finding a spcific word
+            Console.WriteLine("finding a spcific word");
+            Console.WriteLine(findTheBomb("There is a BOMB."));
+            Console.WriteLine(findTheBomb("Hey, did you think there is a bomb?"));
+            Console.WriteLine(findTheBomb("This goes boom!!!"));
+
+            //Convert All Array Items to String
+            object[] arr = { 1, 2, "a", "b" };
+            Console.WriteLine("Convert All Array Items to String");
+            Console.WriteLine(ConvertAllArrayItemstoString(arr));
         }
     }
 }
